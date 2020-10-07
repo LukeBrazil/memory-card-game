17ce69fb74e0105cef6718e9e41565563b84cae3
Luke Brazil <Luke.brazil13@icloud.com>
2020-10-07 10:08:25 -0500

    rendered 16 memory cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MemoryCard from './componets/MemoryCard.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+      <h4>Memory Game</h4>
+
+      </header>
+      <header>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
       </header>
     </div>
   );
